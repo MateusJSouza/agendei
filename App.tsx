40f12c04aaf1f186d2008ccd7f776b707465c840
MateusJSouza <mateusjdev@gmail.com>
2024-10-16 00:28:48 -0300

    feat: rendering doctors cards in the home screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { View } from 'react-native';
 import { Login } from './src/screens/login';
 import { Signup } from './src/screens/signup';
+import { Home } from './src/screens/home';
 
 export default function App() {
   return (
-    <View className='h-screen'>
-      <Signup />
-    </View>
+    <>
+      <Home />
+    </>
   );
 }
