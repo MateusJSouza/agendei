b0d9f1478026524e9bfaf073c637d0dc334e242d
MateusJSouza <mateusjdev@gmail.com>
2024-10-16 01:05:14 -0300

    feat(app): integrating calendar screen into the main application

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import { View } from 'react-native';
 import { Login } from './src/screens/login';
 import { Signup } from './src/screens/signup';
 import { Home } from './src/screens/home';
+import { Calendar } from './src/screens/calendar';
 
 export default function App() {
   return (
     <>
-      <Home />
+      <Calendar />
     </>
   );
 }
