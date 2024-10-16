f740a28ef26db79799daf218bf0d087a21407a68
MateusJSouza <mateusjdev@gmail.com>
2024-10-16 01:04:52 -0300

    feat(calendar): implementing calendar screen to display appointments

---
+++ src/screens/calendar.tsx
@@ -0,0 +1,29 @@
+import { FlatList, Text, View } from "react-native";
+
+import { appointments } from "../constants/data";
+import { CardAppointment } from "../components/card-appointment";
+
+export function Calendar() {
+  return (
+    <View className="flex-1 pt-10">
+      <Text className="text-lg text-black mb-4 ml-4">Agende seus serviços médicos</Text>
+
+      <FlatList
+        className="p-3"
+        data={appointments}
+        keyExtractor={(doctor) => doctor.id_appointment.toString()}
+        renderItem={({ item }) => (
+          <CardAppointment 
+            id_appointment={item.id_appointment}
+            doctor={item.doctor}
+            service={item.service}
+            specialty={item.specialty}
+            booking_date={item.booking_date}
+            booking_hour={item.booking_hour}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  )
+}
