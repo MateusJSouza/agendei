aa50796f6a70f12154125505d06c78f685a2a6c4
MateusJSouza <mateusjdev@gmail.com>
2024-10-16 20:37:40 -0300

    fix archives

--- src/utils/format-date.ts
+++
@@ -1,8 +0,0 @@
-export const formatDate = (dateString: string) => {
-	const date = new Date(dateString);
-	return new Intl.DateTimeFormat("pt-BR", {
-		day: "2-digit",
-		month: "2-digit",
-		year: "numeric",
-	}).format(date);
-};
