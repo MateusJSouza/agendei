fd8460905d335a74989a0377cbc88f75021b5f77
MateusJSouza <mateusjdev@gmail.com>
2024-10-16 14:44:13 -0300

    fix: removing extended colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        blue: '#0D6EFD',
-        black: '#323434',
-        darkGray: '#717F7F',
-        gray: '#A0A0A0',
-        lightGray: '#E4E4E4',
-        white: '#FFFFFF',
-        offWhite: '#F1F5F4',
-        red: '#DF5951',
-      },
-    },
-  },
-  plugins: [],
+	content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 };
