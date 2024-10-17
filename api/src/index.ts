5a1e9e9b0e2fd3aeb9f1756536ad4fa4ef8edfba
MateusJSouza <mateusjdev@gmail.com>
2024-10-17 10:33:30 -0300

    feat: adding first route

---
+++ api/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+import router from './routes'
+
+const app = express()
+
+app.use(cors())
+app.use(router)
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log('Servidor rodando na porta 3001!')
+})
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,14 +1,8 @@
 import { Router } from 'express'
 import controllerDoctor from './controllers/controller.doctor'
-import controllerAppointment from './controllers/controller.appointment'
 
 const router = Router()
 
 router.get('/doctors', controllerDoctor.listDoctors)
-router.get('/appointments', controllerAppointment.list)
-
-router.get('/profile', (req, res) => {
-  res.status(200).send('Aqui vamos listar o perfil do usuário')
-})
 
 export default router
