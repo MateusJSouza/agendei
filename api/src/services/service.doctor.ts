2b4ca803126cadc471b69412844e1bfea900f200
MateusJSouza <mateusjdev@gmail.com>
2024-10-16 23:08:32 -0300

    feat: ensuring proper async handling in listDoctors function in service

---
+++ api/src/services/service.doctor.ts
@@ -0,0 +1,18 @@
+import { v4 as uuidv4 } from 'uuid'
+import repositoryDoctors from '../repositories/repository.doctors'
+
+export interface IDoctor {
+  name: string
+  specialty: string
+  icon: string
+}
+
+async function listDoctors() {
+  const doctors = await repositoryDoctors.listDoctors()
+
+  return doctors
+}
+
+export default {
+  listDoctors,
+}
