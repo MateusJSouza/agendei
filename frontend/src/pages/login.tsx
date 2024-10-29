a48cce59e7a902c885942ca4c38892922937a5e1
MateusJSouza <mateusjdev@gmail.com>
2024-10-29 19:36:54 -0300

    feat: add login and register page

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,45 +1,98 @@
 import logo from '../assets/logo.png'
 import backgroundImage from '../assets/fundo.png'
 import { Link } from 'react-router-dom'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
+import toast, { LoaderIcon } from 'react-hot-toast'
+import { useAuth } from '../hooks/useAuth'
+import type { LoginParams } from '../services/auth/login'
+import auth from '../services/auth'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
+const loginSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: 'O e-mail deve conter no mínimo 1 caracter' })
+    .email('Email inválido'),
+  password: z
+    .string()
+    .min(5, { message: 'A senha deve possuir no mínimo 5 caracteres' }),
+})
+
+type FormData = z.infer<typeof loginSchema>
 
 export function Login() {
+  const {
+    handleSubmit: hookFormSubmit,
+    register,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(loginSchema),
+  })
+
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: async (data: LoginParams) => {
+      return auth.login(data)
+    },
+  })
+
+  const { login } = useAuth()
+
+  const handleSubmit = hookFormSubmit(async data => {
+    try {
+      const { accessToken } = await mutateAsync(data)
+
+      login(accessToken)
+    } catch {
+      toast.error('Credenciais inválidas!')
+    }
+  })
+
   return (
-    <div className="md:grid md:grid-cols-12 flex flex-col gap-4 h-screen">
-      <div className="md:col-span-5 text-center flex items-center justify-center">
-        <form className="w-96 p-4">
+    <div className="flex flex-col gap-4 md:grid md:grid-cols-12 h-screen">
+      <div className="flex justify-center items-center md:col-span-5 text-center">
+        <form className="p-4 w-96" onSubmit={handleSubmit}>
           <img
             src={logo}
             alt="Logo do aplicativo agendei"
-            className="mx-auto w-[170px] mb-4"
+            className="mx-auto mb-4 w-[170px]"
           />
 
           <h5 className="mb-5">
             Gerencie seus agendamentos de forma descomplicada.
           </h5>
-          <h5 className="mb-4 dark:text-zinc-400 text-zinc-300">
+          <h5 className="mb-4 text-zinc-500 dark:text-zinc-400">
             Acesse sua conta
           </h5>
 
-          <div className="flex gap-2 flex-col space-y-2">
-            <input
-              type="email"
-              placeholder="E-mail"
-              className="p-2.5 rounded-md dark:bg-zinc-700 text-zinc-100 focus:outline-none ring-0"
-            />
-            <input
+          <div className="flex flex-col gap-2 space-y-2">
+            <Input type="email" placeholder="E-mail" {...register('email')} />
+            <Input
               type="password"
               placeholder="Senha"
-              className="p-2.5 rounded-md dark:bg-zinc-700 text-zinc-100 focus:outline-none ring-0"
+              {...register('password')}
             />
           </div>
 
+          {errors.email && (
+            <div className="text-red-500">{errors.email.message}</div>
+          )}
+          {errors.password && (
+            <div className="text-red-500">{errors.password.message}</div>
+          )}
+
           <div className="mt-3 mb-5">
-            <button
-              className="rounded-md p-2.5 bg-blue-600 w-full"
+            <Button
+              className="bg-blue-600 p-2.5 rounded-md w-full"
               type="submit"
+              size="lg"
+              disabled={isPending}
             >
-              Login
-            </button>
+              {isPending ? <LoaderIcon /> : 'Login'}
+            </Button>
           </div>
 
           <div>
@@ -55,7 +108,7 @@ export function Login() {
         <img
           src={backgroundImage}
           alt="Imagem de fundo de um médico agendando uma consulta"
-          className="object-cover w-full h-full object-left"
+          className="object-left w-full h-full object-cover"
         />
       </div>
     </div>
--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,71 +1,106 @@
 import logo from '../assets/logo.png'
 import backgroundImage from '../assets/fundo.png'
 import { Link } from 'react-router-dom'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
+import auth from '../services/auth'
+import type { RegisterParams } from '../services/auth/register'
+import toast from 'react-hot-toast'
+import { useAuth } from '../hooks/useAuth'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
+const registerSchema = z
+  .object({
+    name: z.string().min(1, 'Digite seu nome'),
+    email: z.string().min(1, 'Digite seu email').email('Email inválido'),
+    password: z.string().min(4, 'Senha deve ter pelo menos 4 caracteres'),
+    confirmPassword: z.string().min(4),
+  })
+  .refine(data => data.password === data.confirmPassword, {
+    message: 'As senhas não coincidem',
+    path: ['confirmPassword'],
+  })
+
+type FormData = z.infer<typeof registerSchema>
 
 export function Register() {
+  const { handleSubmit: hookFormSubmit, register } = useForm<FormData>({
+    resolver: zodResolver(registerSchema),
+  })
+
+  const { mutateAsync } = useMutation({
+    mutationFn: async (data: RegisterParams) => {
+      return auth.register(data)
+    },
+  })
+
+  const { login } = useAuth()
+
+  const handleSubmit = hookFormSubmit(async data => {
+    try {
+      const { accessToken } = await mutateAsync(data)
+
+      login(accessToken)
+
+      toast.success('Conta criada com sucesso!')
+    } catch {
+      toast.error('Ocorreu um erro ao criar sua conta!')
+    }
+  })
+
   return (
-    <div className="md:grid md:grid-cols-12 flex flex-col gap-4 h-screen">
-      <div className="md:col-span-5 text-center flex items-center justify-center">
-        <form className="w-96 p-4">
-          <img
-            src={logo}
-            alt="Logo do aplicativo agendei"
-            className="mx-auto w-[170px] mb-4"
-          />
+    <div className="flex flex-col gap-4 md:grid md:grid-cols-12 h-screen">
+      <div className="flex flex-col justify-center items-center md:col-span-5 text-center">
+        <img
+          src={logo}
+          alt="Logo do aplicativo agendei"
+          className="mx-auto mb-4 w-[170px]"
+        />
 
-          <h5 className="mb-5">Crie sua conta agora mesmo.</h5>
-          <h5 className="mb-4 dark:text-zinc-400 text-zinc-300">
-            Preencha os campos abaixo
-          </h5>
+        <h5 className="mb-5">Crie sua conta agora mesmo.</h5>
+        <h5 className="mb-4 text-zinc-400">Preencha os campos abaixo</h5>
 
-          <div className="flex gap-2 flex-col space-y-2">
-            <input
-              type="text"
-              placeholder="Nome"
-              className="p-2.5 rounded-md dark:bg-zinc-700 text-zinc-100 focus:outline-none ring-0"
-            />
-            <input
-              type="email"
-              placeholder="E-mail"
-              className="p-2.5 rounded-md dark:bg-zinc-700 text-zinc-100 focus:outline-none ring-0"
-            />
+        <form onSubmit={handleSubmit} className="p-4 w-96">
+          <div className="flex flex-col gap-2 space-y-2">
+            <Input type="text" placeholder="Nome" {...register('name')} />
+            <Input type="email" placeholder="E-mail" {...register('email')} />
 
-            <input
+            <Input
               type="password"
               placeholder="Senha"
-              className="p-2.5 rounded-md dark:bg-zinc-700 text-zinc-100 focus:outline-none ring-0"
+              {...register('password')}
             />
 
-            <input
+            <Input
               type="password"
               placeholder="Confirme a senha"
-              className="p-2.5 rounded-md dark:bg-zinc-700 text-zinc-100 focus:outline-none ring-0"
+              {...register('confirmPassword')}
             />
           </div>
 
-          <div className="mt-3 mb-5">
-            <button
-              className="rounded-md p-2.5 bg-blue-600 w-full"
-              type="submit"
-            >
+          <div className="mt-6 mb-5">
+            <Button size="lg" type="submit">
               Criar minha conta
-            </button>
-          </div>
-
-          <div>
-            <span className="mr-1">Já tenho uma conta.</span>
-            <Link to="/login" className="text-blue-500 underline">
-              Acessar agora!
-            </Link>
+            </Button>
           </div>
         </form>
+
+        <div>
+          <span className="mr-1">Já tenho uma conta.</span>
+          <Link to="/login" className="text-blue-500 underline">
+            Acessar agora!
+          </Link>
+        </div>
       </div>
 
       <div className="md:col-span-7 overflow-hidden">
         <img
           src={backgroundImage}
           alt="Imagem de fundo de um médico agendando uma consulta"
-          className="object-cover w-full h-full object-left"
+          className="object-left w-full h-full object-cover"
         />
       </div>
     </div>
