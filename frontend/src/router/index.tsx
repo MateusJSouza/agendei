c588c0965f0e0c272f092247cff841f62d9bad56
MateusJSouza <mateusjdev@gmail.com>
2024-10-29 19:38:24 -0300

    feat: add routes and http client

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,42 +1,30 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { AuthGuard } from './auth-guard'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from '../pages/login'
 import { Appointments } from '../pages/appointments'
 import { Register } from '../pages/register'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Services } from '../pages/services'
+import { ThemeProvider } from '@/components/theme/theme-provider'
+import { localStorageKeys } from '@/configs/localStorageKeys'
 
 export function Router() {
+  const queryClient = new QueryClient()
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/login"
-          element={
-            <AuthGuard isPrivate={false}>
-              <Login />
-            </AuthGuard>
-          }
-        />
-
-        <Route
-          path="/register"
-          element={
-            <AuthGuard isPrivate={false}>
-              <Register />
-            </AuthGuard>
-          }
-        />
-
-        <Route
-          path="/"
-          element={
-            <AuthGuard isPrivate>
-              <Appointments />
-            </AuthGuard>
-          }
-        />
-
-        <Route path="*" element={<Navigate to="/login" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        defaultTheme="dark"
+        storageKey={localStorageKeys.ACCESS_TOKEN}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/appointments" element={<Appointments />} />
+            <Route path="/services" element={<Services />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
